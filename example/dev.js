c354b1c6b2cabb3ab891018e0515dfbbee473a0b
Harrison Harnisch <hharnisc@gmail.com>
2018-03-01 14:22:34 -0600

    update dev example

--- example/dev.js
+++ example/dev.js
@@ -1,8 +1,16 @@
 const micro = require('micro')
-const { default: EKG, httpGetCheck, dnsResolveCheck } = require('../src')
+const {
+  default: EKG,
+  httpGetCheck,
+  dnsResolveCheck,
+  tcpDialCheck,
+  mongoDBCheck,
+  timeoutCheck,
+} = require('../src')
 
 const ekg = new EKG()
 
+// Liveness
 ekg.addLivenessCheck({
   name: 'passing check',
   check: async () => 'OK',
@@ -21,7 +29,29 @@ ekg.addLivenessCheck({
   name: 'dns buffer',
   check: dnsResolveCheck({ host: 'buffer.com' }),
 })
+ekg.addLivenessCheck({
+  name: 'tcp buffer',
+  check: tcpDialCheck({ host: 'buffer.com', port: 80 }),
+})
+ekg.addLivenessCheck({
+  name: 'mongodb localhost',
+  check: mongoDBCheck({
+    url: 'mongodb://localhost:27017',
+    dbName: 'default',
+  }),
+})
+ekg.addLivenessCheck({
+  name: 'timeout fail!',
+  check: timeoutCheck({
+    check: () =>
+      new Promise(resolve => {
+        setTimeout(resolve, 100)
+      }),
+    timeout: 1,
+  }),
+})
 
+// Readyness
 ekg.addReadynessCheck({
   name: 'passing check',
   check: async () => 'OK',
@@ -39,6 +69,27 @@ ekg.addReadynessCheck({
 ekg.addReadynessCheck({
   name: 'dns buffer',
   check: dnsResolveCheck({ host: 'buffer.com' }),
+})
+ekg.addReadynessCheck({
+  name: 'tcp buffer',
+  check: tcpDialCheck({ host: 'buffer.com', port: 80 }),
+})
+ekg.addReadynessCheck({
+  name: 'mongodb localhost',
+  check: mongoDBCheck({
+    url: 'mongodb://localhost:27017',
+    dbName: 'default',
+  }),
+})
+ekg.addReadynessCheck({
+  name: 'timeout fail!',
+  check: timeoutCheck({
+    check: () =>
+      new Promise(resolve => {
+        setTimeout(resolve, 100)
+      }),
+    timeout: 1,
+  }),
 })
 
 const server = micro(ekg.handler)
